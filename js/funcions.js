8a65f197c825b7366cbd5b4a9fae0d458052838e
Marco <murcojp7@gmail.com>
2024-09-29 20:40:19 +0200

    prova css

--- js/funcions.js
+++ js/funcions.js
@@ -1,3 +1,7 @@
 function confirmaRegistre(){
-    //completa
-}
+    alert("registrant estudiant");
+    document.getElementById("formDiv").innerHTML = "<p class='important'>T'has registrat
+   amb èxit!</p>";
+    console.log("registrant estudiant");
+    return false;
+   }
